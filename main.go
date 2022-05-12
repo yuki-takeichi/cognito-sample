9ec522f0ef528e377d1d894ea49aed5f370a6940
Yuki Takeichi <yuki.takeichi@gmail.com>
2022-05-12 16:09:58 +0900

    impl state

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+
 	// "github.com/rs/cors"
 	"github.com/zitadel/oidc/pkg/client/rp"
+	httphelper "github.com/zitadel/oidc/pkg/http"
 	"github.com/zitadel/oidc/pkg/oidc"
 )
 
@@ -18,7 +21,13 @@ func main() {
 	issuer := "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_FZjE5Kfvl"
 	scopes := []string{"openid", "email", "profile"}
 
-	relyingParty, err := rp.NewRelyingPartyOIDC(issuer, clientID, clientSecret, redirectUrl, scopes)
+	hashKey := "test1234test1234"
+	encryptKey := "test1234test1234"
+	cookieHandler := httphelper.NewCookieHandler([]byte(hashKey), []byte(encryptKey), httphelper.WithUnsecure())
+	options := []rp.Option{
+		rp.WithCookieHandler(cookieHandler),
+	}
+	relyingParty, err := rp.NewRelyingPartyOIDC(issuer, clientID, clientSecret, redirectUrl, scopes, options...)
 	if err != nil {
 		panic(err)
 	}
@@ -28,9 +37,9 @@ func main() {
 	})
 
 	router.Route("/auth", func(r chi.Router) {
-		r.Handle("/login", rp.AuthURLHandler(func() string { return "hoge" }, relyingParty))
+		r.Handle("/login", rp.AuthURLHandler(uuid.NewString, relyingParty))
 		r.Handle("/callback", rp.CodeExchangeHandler(func(w http.ResponseWriter, r *http.Request, tokens *oidc.Tokens, state string, rp rp.RelyingParty) {
-			// w.Write([]byte("done"))
+			// 必要な処理
 			http.Redirect(w, r, "/home", http.StatusFound)
 		}, relyingParty))
 	})
